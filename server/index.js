acafe443fce7ac63a25cd29f63bdda4e6749c63e
girikalra <girikalra@gmail.com>
2018-02-23 18:30:53 -0800

    replace references from old project. minor commit

--- server/index.js
+++ server/index.js
@@ -2,16 +2,10 @@ const express = require('express');
 var bodyParser = require('body-parser');
 let app = express();
 
+
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json())
 
-app.post('/repos', function (req, res) {
-
-});
-
-app.get('/repos', function (req, res) {
-
-});
 
 let port = 4000;
 
