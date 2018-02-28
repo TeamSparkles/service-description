50ab9f29565e91710c7547af12fb65f9bcca2bef
girikalra <girikalra@gmail.com>
2018-02-27 17:19:28 -0800

    Render a slider on page with three hardcoded images

--- client/src/components/photos.jsx
+++ client/src/components/photos.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
 
 class Photos extends React.Component {
   constructor(props) {
@@ -6,18 +7,29 @@ class Photos extends React.Component {
   }
   render() {
 
-    var imgUrl = this.props.photos[0];
+    var imgUrl = this.props.photos[1];
     const imgStyle ={
       width: "600px",
       height: "337.5px",
       backgroundSize: 'cover',
-      backgroundImage: "url('" + imgUrl + "')"
+      backgroundImage: "url('" + imgUrl + "')",
     };
 
     return (
-      <div className= "photosContainer" style={imgStyle}></div>
-    )
+        <div className="photos-container">
+          <Carousel showStatus={false}>
+            <div className="photosCarousel" style={imgStyle}></div>
+            <div className="photosCarousel" style={imgStyle}></div>
+            <div className="photosCarousel" style={imgStyle}></div>
+          </Carousel>
+        </div>
+    );
+
   }
 }
 
 export default Photos;
+
+    // return (
+    //   <div className= "photosContainer" style={imgStyle}></div>
+    // )
