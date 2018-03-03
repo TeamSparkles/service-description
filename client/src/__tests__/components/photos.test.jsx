fde1b81f51d042f2482cdf2ecdb3953913ab798f
girikalra <girikalra@gmail.com>
2018-03-02 21:09:57 -0800

    Add test for photos component

--- client/src/__tests__/components/photos.test.jsx
+++ client/src/__tests__/components/photos.test.jsx
@@ -7,9 +7,13 @@ import Photos from './../../components/Photos';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe ('Description', () => {
+var url1 = 'https://images.pexels.com/photos/245239/pexels-photo-245239.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb';
+var url2 = 'https://images.pexels.com/photos/789818/pexels-photo-789818.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb'
+var photos = [url1, url2];
+
+describe ('Photos', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Description />, div);
+    ReactDOM.render(<Photos photos={photos}/>, div);
   })
 })
